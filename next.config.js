93251256735b9798ad8b2fbe30dff7e87709b0f8
SoftwareMechanic <baruchmanigrasso@gmail.com>
2024-07-14 20:28:14 +0200

    changed conf for 'pino-pretty'

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   // transpilePackages: [
   // '@metaplex-foundation/js']
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
