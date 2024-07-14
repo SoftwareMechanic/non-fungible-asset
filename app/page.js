69d91c893bfe5a8eb02421bcf54d6286d5b8ec79
SoftwareMechanic <baruchmanigrasso@gmail.com>
2024-07-14 20:54:37 +0200

    not show projects if not connected

--- app/page.js
+++ app/page.js
@@ -5,13 +5,25 @@ import styles from "./page.module.css";
 import Navbar from './components/Navbar/Navbar';
 import ProjectsGrid from './components/ProjectsGrid/ProjectsGrid';
 import React, { useState } from 'react';
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export default function Home() {
 
+  const wallet = useWallet()
   return (
     <div>
       <Navbar />
-      <ProjectsGrid />
+      {wallet.connected ? 
+        <ProjectsGrid />
+        :
+        <div style={{ display:"flex", flexDirection:"column", minWidth:"100%", minHeight: "100vh", alignItems: "center", alignContent: "center"}}>
+          <h1> Connect wallet </h1>
+          <WalletMultiButton/>
+        </div>
+
+        
+      }
     </div>
     
   );
